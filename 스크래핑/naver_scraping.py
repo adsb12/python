54bd3900df838647ad2db039a518189e2681bc94
adsb12 <wjswk520@gmail.com>
2024-03-12 15:08:18 +0900

    n_rank

--- 스크래핑/naver_scraping.py
+++ 스크래핑/naver_scraping.py
@@ -3,14 +3,14 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-url ="https://bizforms.co.kr"
+url ="https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EB%84%A4%EC%9D%B4%EB%B2%84+%EC%88%9C%EC%9C%84"
 
 # 엑셀 파일로 저장하기
-filename = "비즈폼 주간업데이트.csv"
+filename = "네이버 음원순위.csv"
 f = open(filename, "w", encoding="utf-8-sig", newline="")
 writer = csv.writer(f)
 
-columns_name = ["순서", "제목"] # 컬럼 속성명 만들기
+columns_name = ["순위", "제목"] # 컬럼 속성명 만들기
 
 writer.writerow(columns_name)
 
@@ -20,8 +20,8 @@ res.raise_for_status()
 
 # soup 객체 만들기
 soup = BeautifulSoup(res.text, "lxml")
-cartoonsBox = soup.find('div', attrs={"class": "recommend_list"}) # 전체 영역에서 'a' 태그를 찾지 않고 인기 급상승 영역으로 범위 제한
-cartoons = cartoonsBox.find_all('span') # 인기 급상승 영역에서 'a'태그 모두 찾아 변수 cartoons에 할당
+cartoonsBox = soup.find('div', attrs={"class": "chart_list_wrap"}) # 전체 영역에서 'a' 태그를 찾지 않고 인기 급상승 영역으로 범위 제한
+cartoons = cartoonsBox.find_all('span', attrs={"class": "txt"}) # 인기 급상승 영역에서 'a'태그 모두 찾아 변수 cartoons에 할당
 # print(cartoons)
 i = 1
 
