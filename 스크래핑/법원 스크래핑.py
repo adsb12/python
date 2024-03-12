8cda3bdfc194e3a40622ee24eb919a7e9a5ef735
adsb12 <wjswk520@gmail.com>
2024-03-12 16:50:28 +0900

    rre

--- 스크래핑/법원 스크래핑.py
+++ 스크래핑/법원 스크래핑.py
@@ -36,6 +36,7 @@ response = requests.post(
     headers=headers,
     data=data,
 )
+response.raise_for_status()
 
 # 엑셀 파일로 저장하기
 filename = "법원경매.csv"
@@ -46,17 +47,13 @@ columns_name = ["순서", "가격"] # 컬럼 속성명 만들기
 
 writer.writerow(columns_name)
 
-# 웹 서버에 요청하기
-response.raise_for_status()
-
 # soup 객체 만들기
 soup = BeautifulSoup(response.text, "lxml")
-costbox = soup.find('table', attrs={"class": "Ltbl_list"}) # 전체 영역에서 'a' 태그를 찾지 않고 인기 급상승 영역으로 범위 제한
-costs = costbox.find_all('td', attrs={"class": "txtright"}) # 인기 급상승 영역에서 'a'태그 모두 찾아 변수 cartoons에 할당
-# print(cartoons)
+costbox = soup.find('table', attrs={"class": "Ltbl_list"}) 
+costs = costbox.find_all('td', attrs={"class": "txtright"}) 
+
 i = 1
 
-# 반복문으로 제목 가져오기(터미널 창 출력 및 엑셀 저장)
 for cost in costs: 
     price = cost.text 
     print(f"{str(i)}위: {price}")
