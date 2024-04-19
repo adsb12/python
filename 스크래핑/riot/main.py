4a23eff970c831f2b2f8092fc9633e2750759193
adsb12 <wjswk520@gmail.com>
2024-04-19 16:44:18 +0900

    riot3

--- 스크래핑/riot/main.py
+++ 스크래핑/riot/main.py
@@ -1,26 +1,26 @@
 
-from flask import Flask, render_template, request, redirect, send_file
+from flask import Flask, render_template, request, redirect
 from riot import riot_crowl
+#함수 가져오기
 
-
-app = Flask("riot")
-db = {} # 데이터베이스
+app = Flask("riot")         #어플리케이션 패키지의 이름
+db = {}                         # 데이터베이스 실제 db는 아님 성능 향상때매
 
 @app.route("/")
 def home():
-  return render_template("home.html")
+  return render_template("home.html")       #기본루트
 
-@app.route("/search")
+@app.route("/search")                               #검색
 def search():
-  name = request.args.get("name")
+  name = request.args.get("name")             #작성한 이름들고오기
   if name == None:
-    return redirect("/")
+    return redirect("/")                                #없으면 다시빽
   if name in db: 
-    r_lists = db[name]
+    r_lists = db[name]                                 #db에 있으면 db있는거 뿌림
   else:
-    rank,r_len = riot_crowl(name)
+    rank,r_len = riot_crowl(name)                 #없으면 만든함수 호출
     r_lists = rank
     db[name] = r_lists 
-  return render_template("search.html", name = name, r_lists=r_lists, r_len=r_len)
+  return render_template("search.html", name = name, r_lists=r_lists, r_len=r_len)      #html로 던짐
 
 app.run("0.0.0.0")
--- 스크래핑/riot/riot.py
+++ 스크래핑/riot/riot.py
@@ -3,8 +3,10 @@ from requests import get
 def riot_crowl(name):
     
     api_key = "RGAPI-0c58aaca-2526-49e4-bde8-9294a2e4e69b"
+    #api 키
     
     base_url = f"https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}"
+    #api센터에 있던 url에서 마지막 값은 입력 할 값이니 변수를 넣어준다
     
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
@@ -13,20 +15,24 @@ def riot_crowl(name):
         "Origin": "https://developer.riotgames.com",
         "X-Riot-Token": api_key
     }
+    #header값을 들고 오는데 토큰값만 자기껄로 변경 해주자
     
     res = get(url=base_url,headers=headers)
+    #riot_api에 주소를 던지고 헤더값도 던진다
     
-    if res.status_code != 200:
+    if res.status_code != 200:              #실패하면 밑에 출력
         print("잘못 받아옴")
     else:
         enc_id = res.json()['id']
+        #받아온 값 중에 필요한건 암호화된 id이니까 그것만 가져온다
         
         riot_url = f"https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{enc_id}"
+        #그리고 league-v4에 위와 똑같이 값을 던진다
         
-        riot_res = get(url=riot_url,headers=headers)
-        riot_lists = riot_res.json()
+        riot_res = get(url=riot_url,headers=headers) #똑같이 가져와주고
+        riot_lists = riot_res.json()                             #json형식으로 담는다
         
-        def get_riot_list(riot_list):
+        def get_riot_list(riot_list):                             #배열에 담기 위한 함수 선언
             r_res = [
                 riot_list.get('queueType'),
                 riot_list.get('tier'),
@@ -40,7 +46,7 @@ def riot_crowl(name):
         
         results = []
         for riot_list in riot_lists:
-            results.append(get_riot_list(riot_list))
-        length = len(results)
+            results.append(get_riot_list(riot_list))        #results에 결과값 저장
+        length = len(results)                                     #나중에 값이 없을때를 대비한 것
         
         return results,length
